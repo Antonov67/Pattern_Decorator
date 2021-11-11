34e629c950754602f4a7298d53efb1cc874b0f91
Dmitriy Antonov <79038945169@yandex.ru>
2021-11-11 16:17:28 +0300

    3

--- src/com/company/CondimentDecorator.java
+++ src/com/company/CondimentDecorator.java
@@ -1,6 +1,11 @@
 package com.company;
 
 public abstract class CondimentDecorator extends Beverage{
+    public Beverage beverage;
     @Override
     public abstract String getDescription();
+
+    public Size getSize(){
+        return beverage.getSize();
+    }
 }
